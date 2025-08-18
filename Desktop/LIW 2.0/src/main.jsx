30fd813b18b0c2b2c8a74311c2328dd182999c6d
estebanampuero <31929548+estebanampuero@users.noreply.github.com>
2025-08-17 23:52:19 -0400

    adddd

--- Desktop/LIW 2.0/src/main.jsx
+++ Desktop/LIW 2.0/src/main.jsx
@@ -1,15 +1,13 @@
-// src/main.jsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx' // <-- ¿Está importando el App.jsx correcto?
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /> {/* <-- ¿Está renderizando el componente App? */}
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
